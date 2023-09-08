a5a09a53ba96c04fc8a0054dfbe98867a610dba4
deniel <papp.dani2206@gmail.com>
2023-09-08 09:28:12 +0200

    valtozok_feladat_01

---
+++ 00_Valtozok/solution_valtozo/feladat_00/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Katarzyna Skowronska-Dolata";
+string nationality = "Lengyel";
+int birthDate = 1983;
+string birthPlace = "Warszava";
+double height = 1.89;
+int weight = 75;
+int hitHeight = 317;
+int rampartHeight = 302;
+int playerCount = 2;
+bool captain = false;
+char captainMark = 'C';
+
+Console.ReadKey();
---
+++ 00_Valtozok/solution_valtozo/feladat_01/Program.cs
@@ -0,0 +1,4 @@
+﻿int year = 1970;
+string model = "Torino Cobra SportsRoof";
+
+Console.WriteLine($"A Ford autógyár {year}-ben gyártotta le a {model} modellt.");
