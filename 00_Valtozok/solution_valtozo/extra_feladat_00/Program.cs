29b96171d0e9b5a1f4dc097ff0498403dff6d4aa
deniel <papp.dani2206@gmail.com>
2023-09-11 22:05:59 +0200

    hazi0911

---
+++ 00_Valtozok/solution_valtozo/extra_feladat_00/Program.cs
@@ -0,0 +1,4 @@
+﻿string brand = "Mazda";
+string modell = "RX-8";
+
+Console.WriteLine($"A {brand} 2012-ben gyártotta le az utolsó {modell}-at.");
---
+++ 00_Valtozok/solution_valtozo/extra_feladat_01/Program.cs
@@ -0,0 +1,6 @@
+﻿string schoolName = "Vasvári Pál";
+int classNumber = 10;
+char className = 'b';
+
+Console.WriteLine($"A {schoolName} iskola szoft. fejl. és tesztelő {classNumber}-{className} osztályába járok");
+
