f6d92bcd7a0f9a286996147cf4ba709b04a704f4
deniel <papp.dani2206@gmail.com>
2023-09-12 07:35:21 +0200

    0912

---
+++ 00_Valtozok/solution_valtozo/feladat_03/Program.cs
@@ -0,0 +1,7 @@
+﻿string dynasty = "Ming";
+int startYear = 1368;
+int endYear 1644;
+string wall = "Kínai Nagyfal";
+int wallLenght = 7200;
+
+Console.WriteLine($"A {dynasty} dinasztia {startYear} és {endYear} közt építette a ma látható {wall} {wallLenght}km hosszú szakaszát");
---
+++ 00_Valtozok/solution_valtozo/feladat_04/Program.cs
@@ -0,0 +1,7 @@
+﻿int year = 2008;
+string brand = "Sukhoj";
+string modell = "SU-35S";
+double speed = 2.4;
+string measureUnit = "m/s";
+
+Console.WriteLine($"Oroszország legmodernebb vadászrepülőgépe a {year}-ban bemutatott {brand} gyártotta {modell} amely képes a {speed} Mach sebességre, ami 720{measureUnit}-nak felel meg");
