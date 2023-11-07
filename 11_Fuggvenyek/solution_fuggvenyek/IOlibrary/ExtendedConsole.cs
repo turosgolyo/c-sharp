1792970167c15375d7d0a6218a2c779acf0e7c06
deniel <papp.dani2206@gmail.com>
2023-11-07 09:25:44 +0100

    fuggvenyek_00
    sadam husein :3

---
+++ 11_Fuggvenyek/solution_fuggvenyek/IOlibrary/ExtendedConsole.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace IOLibrary;
+
+public static class ExtendedConsole
+{
+    public static int ReadInteger()
+    {
+        bool isNumber = false;
+        int number = 0;
+
+        do
+        {
+            string text = Console.ReadLine();
+            isNumber = int.TryParse(text, out number);
+        }
+        while (!isNumber);
+
+        return number;
+        
+    }
+
+    public static double ReadDouble()
+    {
+        bool isNumber = false;
+        double number = 0;
+
+        do
+        {
+            string text = Console.ReadLine();
+            isNumber = double.TryParse(text, new CultureInfo("en-EN"), out number);
+        }
+        while (!isNumber);
+
+        return number;
+
+    }
+}
---
+++ 11_Fuggvenyek/solution_fuggvenyek/feladat_00/Program.cs
@@ -0,0 +1,9 @@
+﻿using IOLibrary;
+using System;
+
+Console.WriteLine("Adjon meg egy számot!");
+int number = ExtendedConsole.ReadInteger();
+number.WriteToConsole();
+
+Console.ReadKey();
+
