6eac5071fefa988e3f5d0c6f8d52a0c0221aeed0
turosgolyo <papp.dani2206@gmail.com>
2023-11-13 22:45:41 +0100

    ciklusok_08
    szampiramis

---
+++ 05_Ciklusok/solution_ciklusok_for/feladat_20/Program.cs
@@ -0,0 +1,16 @@
+﻿int rows = 0;
+int space = 0;
+int number = 0;
+
+Console.WriteLine("Adja meg a számpiramis sorainak számát!");
+rows = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= rows; i++)
+{
+    for (number = rows; number >= i; number--)
+    {
+        Console.Write(number);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
---
+++ 05_Ciklusok/solution_ciklusok_for/feladat_21/Program.cs
@@ -0,0 +1,25 @@
+﻿int rows = 0;
+int space = 0;
+int number = 0;
+
+Console.WriteLine("Adja meg a számpiramis sorainak számát!");
+rows = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= rows; i++)
+{
+    for (space = 1; space <= (rows - i); space++)
+    {
+        Console.Write("  ");
+    }
+    for (number = 1; number <= i; number++)
+    {
+        Console.Write(number);
+        Console.Write(" ");
+    }
+    for (number = (i - 1); number >= 1; number--)
+    {
+        Console.Write(number);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
