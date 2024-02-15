1a181ef4fd3616cae0d64774e6bc6b0e0a2ea71f
deniel <papp.dani2206@gmail.com>
2024-02-15 09:38:50 +0100

    ez a cisarp trokodik velm

--- 09 - Collections/01_FileRead/FileService.cs
+++ 09 - Collections/01_FileRead/FileService.cs
@@ -1,8 +1,11 @@
 ﻿
 
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 public static class FileService
 {
-    public static async Task<List<Student>> ReadFromFileAsync(string fileName) 
+    #region File Read
+    public static async Task<List<Student>> ReadFromFileV1Async(string fileName) 
     { 
         List<Student> students = new List<Student>();
         Student student = null;
@@ -13,6 +16,8 @@ public static class FileService
 
         using FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None);
         using StreamReader sr = new StreamReader(fs, Encoding.UTF7);
+
+        await sr.ReadLineAsync();
 
         //ha nincs using akkor 
         //fs.Close();
@@ -32,4 +37,130 @@ public static class FileService
 
         return students;
     }
+    public static async Task<List<Student>> ReadFromFileV2Async(string fileName)
+    {
+        List<Student> students = new List<Student>();
+        Student student = null;
+        string[] data = null;
+
+        string path = Path.Combine("source", fileName);
+
+        string[] lines = await File.ReadAllLinesAsync(path, Encoding.UTF7);
+
+        foreach(string line in lines.Skip(1))
+        {
+            data = line.Split("\t");
+
+            student = new Student();
+            student.Name = data[0];
+            student.Average = double.Parse(data[1]);
+
+            students.Add(student);
+        }
+
+        return students;
+    }
+    public static async Task<List<Student>> ReadFromFileV3Async(string fileName)
+    {
+        List<Student> students = new List<Student>();
+        Student student = null;
+        string[] data = null;
+        bool isNumber = false;
+
+        string path = Path.Combine("source", fileName);
+
+        IAsyncEnumerable<string> lines = File.ReadLinesAsync(path, Encoding.UTF7);
+
+        await foreach (string line in lines)
+        {
+            data = line.Split("\t");
+
+            isNumber = double.TryParse(data[1], out double average);
+            if (!isNumber)
+            {
+                continue;
+            }
+            
+            student = new Student();
+            student.Name = data[0];
+            student.Average = average;
+
+            students.Add(student);
+        }
+
+        return students;
+    }
+    public static async Task<List<Student>> ReadFromFileV4Async(string fileName)
+    {
+        List<Student> students = new List<Student>();
+        Student student = null;
+        string[] data = null;
+
+        string path = Path.Combine("source", fileName);
+
+        string text = File.ReadAllText(path, Encoding.UTF7);
+
+        string[] lines = text.Split("\n");
+        
+        foreach (string line in lines.Skip(1))
+        {
+            data = line.Split("\t");
+
+            student = new Student();
+            student.Name = data[0];
+            student.Average = double.Parse(data[1]);
+
+            students.Add(student);
+        }
+
+        return students;
+    }
+    #endregion
+
+    #region File Write
+    public static async Task WriteToFileV1Async(string fileName, ICollection<Student> students)
+    {
+        Directory.CreateDirectory("output");
+
+        string path = Path.Combine("output", $"{fileName}.txt");
+
+        using FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None, 128);
+        using StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+        foreach(Student student in students)
+        {
+            await sw.WriteLineAsync($"{student.Name}\t{student.Average}");
+        }
+    }
+    public static async Task WriteToFileV2Async(string fileName, ICollection<Student> students)
+    {
+        Directory.CreateDirectory("output");
+
+        string path = Path.Combine("output", $"{fileName}.txt");
+
+        List<string> data = new List<string>();
+
+        foreach (Student student in students)
+        {
+            data.Add($"{student.Name}\t{student.Average}");
+        }
+
+        await File.WriteAllLinesAsync(path, data, Encoding.UTF8);
+    }
+    public static async Task WriteToFileV3Async(string fileName, ICollection<Student> students)
+    {
+        Directory.CreateDirectory("output");
+
+        string path = Path.Combine("output", $"{fileName}.txt");
+
+        StringBuilder contents = new StringBuilder();
+
+        foreach(Student student in students)
+        {
+            contents.AppendLine($"{student.Name}\t{student.Average}");
+        }
+
+        await File.WriteAllTextAsync(path, contents.ToString(), Encoding.UTF8);
+    }
+    #endregion
 }
--- 09 - Collections/01_FileRead/Program.cs
+++ 09 - Collections/01_FileRead/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿List<Student> students = await FileService.ReadFromFileV2Async("adatok.txt");
+
+Console.ReadKey();
+
+students.WriteCollectionToConsole();
+
+Console.WriteLine($"az osztalyban {students.Count} diák jár");
+
+
+double average = students.Average(x => x.Average);
+Console.WriteLine($"az osztaly átlaga {average:F2}");
+
+Student bestStudent = students.MaxBy(x => x.Average);
+Console.WriteLine($"A legjobb tanuló {bestStudent}");
+Console.WriteLine();
+
+List<Student> studentsAboveAverage = students.Where(x => x.Average > average)
+											 .ToList();
+
+await FileService.WriteToFileV1Async("atlagfelett", studentsAboveAverage);
+
+bool hasExcellentStudent = students.Any(x => x.Average == 5);
+Console.WriteLine($"{(hasExcellentStudent ? "Van" : "Nincs")}");
+
+/*
+Feladatunk, hogy
+1 - Írjuk ki minden diák adatát a képernyőre!
+2 - Hány diák jár az osztályba?
+3 - Mennyi az osztály átlaga?
+4 - Keressük a legtöbb pontot elért érettségizőt és írjuk ki az adatait a képernyőre.
+5 - atlagfelett.txt allományba keressük ki azon tanulókat kiknek pontjai meghaladják az átlagot!
+6 - Van e kitünő tanulónk?
+7 - Hány elégtelen, elégséges, jó, jeles és kitünő tanuló van az osztályban?
+    Értékhatárok:
+	- elégtelen, ha: 0.00 - 1.99
+	- elégséges, ha: 2.00 - 2.99
+	- jó, ha: 3.00 - 3.99
+	- jeles, ha: 4.00 - 4.99
+	- kitünő, ha: 5.00
+*/
