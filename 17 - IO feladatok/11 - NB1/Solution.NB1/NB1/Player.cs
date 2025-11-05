2bd9069bdd387898433fffebbab5446b65248b98
turosgolyo <papp.dani2206@gmail.com>
2025-11-05 09:30:36 +0100

    nb1

---
+++ 17 - IO feladatok/11 - NB1/Solution.NB1/NB1/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace NB1;
+
+public class Player
+{
+    public string ClubName { get; set; }
+    public int Number { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public bool IsHungarian { get; set; }
+    public bool IsForeign { get; set; }
+    public int Worth { get; set; }
+    public string Postion { get; set; }
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} ({BirthDate})";
+    }
+
+}
