a9136fac205e8ab6895cc813f8072500dcb5920c
turosgolyo <papp.dani2206@gmail.com>
2026-01-23 08:45:27 +0100

    meterologia

---
+++ Meteorologia/Meteorologia/Program.cs
@@ -0,0 +1,30 @@
+﻿using Meteorologia;
+using System.Globalization;
+using System.Text;
+
+List<string> lines = File.ReadAllLines("tavirathu13.txt", Encoding.UTF7).ToList();
+List<Weather> weathers = new List<Weather>();
+
+foreach (string line in lines)
+{
+    List<string> data = line.Split(' ').ToList();
+    weathers.Add(new Weather
+    {
+        City = data[0],
+        Time = DateTime.ParseExact(data[1], "HHmm", CultureInfo.InvariantCulture),
+        Wind = data[2],
+        Temperature = int.Parse(data[3]),
+    });
+}
+
+Console.WriteLine("Adjon meg egy varos kodjat: ");
+string city = Console.ReadLine().ToUpper();
+
+Weather latestMeasure = weathers.Where(x => x.City == city).OrderByDescending(x => x.Time).FirstOrDefault();
+Console.WriteLine(latestMeasure.ToString());
+
+Weather min = weathers.FirstOrDefault(x => x.Temperature == weathers.Min(x => x.Temperature));
+Console.WriteLine($"{min.City} {min.Time} {min.Temperature}");
+Weather max = weathers.FirstOrDefault(x => x.Temperature == weathers.Max(x => x.Temperature));
+Console.WriteLine($"{max.City} {max.Time} {max.Temperature}");
+
---
+++ Meteorologia/Meteorologia/Weather.cs
@@ -0,0 +1,14 @@
+﻿namespace Meteorologia;
+
+public class Weather
+{
+    public string City { get; set; }
+    public DateTime Time { get; set; }
+    public string Wind { get; set; }
+    public int Temperature { get; set; }
+
+    public override string ToString()
+    {
+        return $"{City} - {Time.ToString("HH:mm")}: {Wind} - {Temperature} °C";
+    }
+}
